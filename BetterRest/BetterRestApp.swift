11bc40205550340f961e66a2117ba6383409de4f
Tensai Solutions <sidellp@gmail.com>
2023-10-16 18:38:29 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,18 @@
+//
+// BetterRestApp.swift
+// BetterRest
+//
+// Created by Tensai Solutions on 10/16/23
+// Copyright © 2023 Tensai Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
